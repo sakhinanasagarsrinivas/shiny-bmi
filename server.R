a8538a4c989eaf38d01a1ce7f1e5aa309dc9d3c6
Alexander Seifert <alexander.seifert@gmail.com>
2014-06-05 19:53:31 +0200

    refactored the bmi calculation to its own file

--- server.R
+++ server.R
@@ -1,15 +1,4 @@
-METRIC   = 1;
-IMPERIAL = 2;
-
-bmi <- function(mass, height, units) {
-        bmi = mass / height^2
-        
-        if (units == IMPERIAL) {
-                bmi = bmi * 703
-        }
-        
-        return (bmi)
-}
+source("./bmi.R")
 
 shinyServer(function(input, output) {
         output$input <- renderText({
